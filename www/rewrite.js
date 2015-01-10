32ad498c828774f2d41e7a45283fee4447355582
Nick Bray <ncbray@gmail.com>
2015-01-10 10:31:58 -0800

    UI improvements.

--- www/rewrite.js
+++ www/rewrite.js
@@ -30,10 +30,11 @@ rewrite.init = function() {
 		    var f = data.Files[i];
 		    output.append('<div><b>' + f.Path + '</b></div>');
 		    if (f.Lines.length) {
-			var container = $('<div />').css('padding', 10).css('background-color', '#eee');
+			var container = $('<div class="filediff"/>');
 			for (j in f.Lines) {
 			    var line = f.Lines[j];
-			    var original = $('<div><pre>' + line.Text + '</pre></div>');
+			    console.log(line);
+			    var original = $('<div><span class="lineno">' + (line.Line + 1) + '</span><span class="code">' + line.Text + '</span></div>');
 			    original.css('color', '#040');
 			    container.append(original);
 			    if (line.Rewritten) {
@@ -59,21 +60,25 @@ rewrite.init = function() {
 
     var form = $('<form>');
 
-    var directory = $('<input type="text" name"Directory" placeholder="Directory" />');
+    var directory = $('<input type="text" name"Directory" size=45 placeholder="Directory" />');
     refreshOnEnter(directory);
     form.append(directory);
 
-    var file_suffixes = $('<input type="text" name="FileSuffixes" placeholder="File sufixes" />');
+    var file_suffixes = $('<input type="text" name="FileSuffixes" size=45 placeholder="File sufixes" />');
     refreshOnEnter(file_suffixes);
     form.append(file_suffixes);
 
-    var content_regex = $('<input type="text" name="MatchContent" placeholder="Content regex" />');
+    var content_regex = $('<input type="text" name="MatchContent" size=100 placeholder="Content regex" />');
     refreshOnEnter(content_regex);
-    form.append(content_regex);
+    var div = $('<div />');
+    div.append(content_regex);
+    form.append(div);
 
-    var content_replace = $('<input type="text" name="MatchContent" placeholder="Content replacement" />');
+    var content_replace = $('<input type="text" name="MatchContent" size=100 placeholder="Content replacement" />');
     refreshOnEnter(content_replace);
-    form.append(content_replace);
+    div = $('<div />');
+    div.append(content_replace);
+    form.append(div);
 
 
     var button = $('<input type="button" value="Refresh" />');
